84e271f231d87fce715b2b3a9b8b0c51d741a270
Nurislom373 <nurislomx72@gmail.com>
2022-05-01 16:49:10 +0500

    Add junit jupiter for testing

---
+++ src/test/java/elbar/company/learn_center_rest/service/auth/role/RoleServiceImplTest.java
@@ -0,0 +1,39 @@
+package elbar.company.learn_center_rest.service.auth.role;
+
+import elbar.company.learn_center_rest.response.Data;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleServiceImplTest {
+
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void detail() {
+    }
+
+    @Test
+    void list() {
+    }
+}
