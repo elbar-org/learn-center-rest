c1d84f26a7a640a2f62d1e6f7dceab7078b421f1
Nurislom373 <nurislomx72@gmail.com>
2022-05-11 17:04:18 +0500

    AuthUser Dtos fill fields

--- src/main/java/elbar/company/learn_center_rest/dto/auth/user/AuthUserDetailDTO.java
+++ src/main/java/elbar/company/learn_center_rest/dto/auth/user/AuthUserDetailDTO.java
@@ -3,11 +3,28 @@ package elbar.company.learn_center_rest.dto.auth.user;
 import elbar.company.learn_center_rest.dto.GenericDTO;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthUserDetailDTO extends GenericDTO {
-    private String test;
+    @NotBlank(message = "{user.username.required}")
+    private String username;
+
+    @NotBlank(message = "{user.password.required}")
+    private String password;
+
+    @NotBlank(message = "{user.email.required}")
+    private String email;
+
+    private String image;
+
+    @NotBlank(message = "{user.language.required}")
+    private String language;
+
+    @NotBlank(message = "{user.status.required}")
+    private String status;
 }
--- src/main/java/elbar/company/learn_center_rest/dto/auth/user/AuthUserLoginDTO.java
+++ src/main/java/elbar/company/learn_center_rest/dto/auth/user/AuthUserLoginDTO.java
@@ -3,12 +3,16 @@ package elbar.company.learn_center_rest.dto.auth.user;
 import elbar.company.learn_center_rest.dto.BaseDTO;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthUserLoginDTO implements BaseDTO {
+    @NotBlank(message = "{user.username.required}")
     private String username;
+    @NotBlank(message = "{user.password.required}")
     private String password;
 }
