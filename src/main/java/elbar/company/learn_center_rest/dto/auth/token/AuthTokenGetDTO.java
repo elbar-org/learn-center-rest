9ff1ae5e220182aef7812332d3f3512541704162
Nurislom373 <nurislomx72@gmail.com>
2022-05-12 14:55:55 +0500

    Token Dtos fill fields

--- src/main/java/elbar/company/learn_center_rest/dto/auth/token/AuthTokenGetDTO.java
+++ src/main/java/elbar/company/learn_center_rest/dto/auth/token/AuthTokenGetDTO.java
@@ -12,11 +12,9 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthTokenGetDTO extends GenericDTO {
-    @Min(value = 5, message = "{type.min.size}")
     @NotBlank(message = "{type.required}")
     private String type;
 
-    @Min(value = 5, message = "{token.min.size}")
     @NotBlank(message = "{token.required}")
     private String token;
 }
